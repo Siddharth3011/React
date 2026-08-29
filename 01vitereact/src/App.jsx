2bb0b05c07f4001947a36d78540f5124a4315257
Siddharth3011 <sidpandey.codes@gmail.com>
2026-08-30 00:02:46 +0530

    fixed some comments in app.jsx

--- 01vitereact/src/App.jsx
+++ 01vitereact/src/App.jsx
@@ -8,7 +8,7 @@ function App() {
     <Chai />
     <h1>Hello {username}</h1> {/* here {username} is a evaluated expression. */}
     <p>Lorem ipsum dolor sit amet.</p>
-    </> //we can only return one parent element so we use React fragments.
+    </> //in jsx we can only return one parent element so we use React fragments to return more.
     //it is like we write a div but we don't want to write div many times. so we remove the <div>...</div> and write <>...</>.
   )
 }
